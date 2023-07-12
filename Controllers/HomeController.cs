56a0859711a2ed3a4d6d706594d3840de4c36e31
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-12 14:43:11 +0300

    replace file(rsvpform) of second view(old not finding ), add link to index pag, add a form to second page(rsvpform), add repository (class file) to keeping data of results of form, add to store data from page rsvpform,add page to thanks visitor(thanks)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,14 +30,23 @@
 //    }
 //}
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
 namespace PartyInvites.Controllers;
 public class HomeController : Controller {
     public IActionResult Index()
     {
         return View();
     }
+    [HttpGet]
     public ViewResult RsvpForm() 
     {
         return View();
+    }
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+        Repository.AddResponse(guestResponse);
+        return View("Thanks",guestResponse);
     }
 }
